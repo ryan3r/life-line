e0f902ac6591f239d6e1026470381a74a587db16
Ryan <ryan314r@gmail.com>
2017-01-28 17:22:47 -0600

    Fix issue where 'undefined' is displayed in the input field

--- src/client/widgets/input.js
+++ src/client/widgets/input.js
@@ -9,6 +9,9 @@ lifeLine.makeDom.register("input", {
 			value = bind[prop];
 		}
 
+		// done put the literal undefined in if no value is given
+		value || (value = "");
+
 		var input = {
 			tag: tag || "input",
 			classes: classes || `${tag == "textarea" ? "textarea" : "input"}-fill`,
