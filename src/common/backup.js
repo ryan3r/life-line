38fe9a4fb00624e9c4dfb31cfbbfc04962ad0bab
Ryan <ryan314r@gmail.com>
2017-07-14 00:38:31 -0500

    Initial setup

--- src/common/backup.js
+++
@@ -1,8 +0,0 @@
-/**
- * Name generator for backups
- */
-
-exports.genBackupName = function(date = new Date()) {
-	return `backup-${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
-		+ `-${date.getHours()}-${date.getMinutes()}.zip`;
-};
