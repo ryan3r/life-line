d0eed3d6abfa6bc04d990b75349307afdaad0370
Ryan <ryan314r@gmail.com>
2017-08-23 21:27:40 -0500

    Update the Events class to support getting the current value and sending changes.

--- src/util/events.js
+++ src/util/events.js
@@ -1,43 +1,75 @@
 import Subscription from "./subscription";
 
 export default class Events {
-	constructor(tag) {
-		this._listeners = new Map();
+	constructor() {
+		this._listeners = {}
+	}
+
+	// define an event
+	defineEvent(name, initializeWith) {
+		// the listeners for this event
+		let listeners = [];
+		// save the listeners
+		this._listeners[name] = listeners;
+
+		// add a listener
+		this[`on${name}`] = fn => {
+			// emit a value to the listeners
+			const emitValue = value => {
+				value = initializeWith ? this[initializeWith] : value;
+
+				// resolve the promise them send the value
+				if(value instanceof Promise) {
+					value.then(value => {
+						// check that we still wan this value
+						if(subscription.active) {
+							fn(value);
+						}
+					});
+				}
+				// emit the value
+				else {
+					fn(value);
+				}
+			};
+
+			// add the actual listener
+			let subscription = this._on(name, emitValue);
+
+			// send the initial value
+			if(initializeWith) {
+				emitValue();
+			}
+
+			return subscription;
+		};
 	}
 
 	// listen to an event of type {type}
-	on(type, fn) {
-		// make sure we have this type of listener registered
-		if(!this._listeners.has(type)) {
-			this._listeners.set(type, []);
-		}
-
+	_on(type, fn) {
 		// add the listener
-		this._listeners.get(type).push(fn);
+		this._listeners[type].push(fn);
 
 		// create a subscription for this listener
 		return new Subscription(() => {
-			const index = this._listeners.get(type).indexOf(fn);
+			const index = this._listeners[type].indexOf(fn);
 
 			// remove the listener
 			if(index !== -1) {
-				this._listeners.get(type).splice(index, 1);
+				this._listeners[type].splice(index, 1);
 			}
 		});
 	}
 
 	// emit an event of type {type} with the arguments {args}
-	emit(type, ...args) {
-		// check if we have any listeners for this type
-		if(this._listeners.has(type)) {
-			for(const fn of this._listeners.get(type)) {
-				fn(...args);
-			}
+	emit(type, arg) {
+		for(const fn of this._listeners[type]) {
+			fn(arg);
 		}
 	}
 
 	// check if an event has any listeners attached
 	hasListeners(type) {
-		return this._listeners.has(type) && this._listeners.get(type).length > 0;
+		return this._listeners[type] && this._listeners[type].length > 0;
 	}
 }
