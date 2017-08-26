e6f9f8612314f75770e5f0435fa8420080833ecd
Ryan <ryan314r@gmail.com>
2017-08-26 13:38:44 -0500

    Upgrade store to the new version of Events.

--- src/stores/store.js
+++ src/stores/store.js
@@ -4,36 +4,27 @@ export default class Store extends Events {
 	constructor(name, state) {
 		super();
 
+		this.defineEvent("StateChange", "state");
+
 		this.name = name;
-		this._state = state;
+		this.value = value;
 	}
 
 	// change the state
 	set(state) {
-		this._state = state;
+		this.state = state;
 
-		this.emit("state-change", state);
-	}
-
-	// get the current state and listen for changes
-	onStateChange(fn) {
-		// emit the current state
-		fn(this._state);
-
-		return this.on("state-change", fn);
+		this.emit("StateChange");
 	}
 
 	// bind a store to the react state
 	bind(component, name = this.name) {
-		// set the current state in the component
-		component.state[name] = this._state;
-
 		// listen for state changes
 		component.addSub(
-			this.on("state-change", () => {
+			this.on("state-change", state => {
 				// update the component's state
 				component.setState({
-					[name]: this._state
+					[name]: state
 				});
 			})
 		);
