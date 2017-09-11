68974f556157ac61d7e3cd01820c61ea470cf7b8
Ryan <ryan314r@gmail.com>
2017-09-10 21:40:13 -0500

    More useful date coloring.

--- src/ui/task-components/task-prop.js
+++ src/ui/task-components/task-prop.js
@@ -2,6 +2,7 @@ import TaskComponent from "./task-component";
 import React from "react";
 const moment = require("moment");
 import capitalizeFirst from "../../util/capitalize-first";
+import {TASK_PROPS} from "../../constants";
 
 export default class TaskProp extends TaskComponent {
 	addListeners() {
@@ -24,8 +25,11 @@ export default class TaskProp extends TaskComponent {
 		// nothing to show
 		if(!this.state.value) return null;
 
+		// find the current task prop
+		const propDef = TASK_PROPS.find(prop => prop.name == this.props.prop);
+
 		// display the description
-		if(this.props.prop == "description") {
+		if(propDef.editor == "textarea") {
 			return <div>
 				{/* Make each line a div */}
 				{this.state.value.split("\n").map((line, i) => {
@@ -35,9 +39,29 @@ export default class TaskProp extends TaskComponent {
 		}
 
 		// display the date
-		if(this.props.prop == "due") {
-			return <div style={{color: "purple", marginBottom: 15}}>
-				due {moment(this.state.value).fromNow()}
+		if(propDef.editor == "date") {
+			const date = moment(this.state.value);
+			// pick the color for this date
+			let color = "black";
+			// the number of days from now
+			const days = date.diff(moment(), "days");
+
+			// overdue is red
+			if(date.isSameOrBefore(moment(), "days")) {
+				color = "red";
+			}
+			// next few days is yellow
+			else if(days <= 2) {
+				color = "orange";
+			}
+			// next 5 days is cyan
+			else if(days <= 5) {
+				color = "#0dd";
+			}
+
+			// display the due date
+			return <div style={{color, marginBottom: 15}}>
+				due {date.fromNow()}
 			</div>;
 		}
 
