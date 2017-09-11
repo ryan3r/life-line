2182ea0ed32ab9e57b00fa31ffc3278663db6326
Ryan <ryan314r@gmail.com>
2017-09-10 21:19:57 -0500

    Use the correct export format.

--- src/util/capitalize-first.js
+++ src/util/capitalize-first.js
@@ -1,4 +1,6 @@
 // capitalize the first letter
-export default const capitalizeFirst = word => {
+const capitalizeFirst = word => {
 	return word.charAt(0).toUpperCase() + word.substr(1);
 };
+
+export default capitalizeFirst;
